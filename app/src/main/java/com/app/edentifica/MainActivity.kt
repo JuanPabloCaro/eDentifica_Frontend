d3d8ce7821c5d9a4d7916a9adbf1e7480f33794a
jcaro <jcaropenuela@gmail.com>
2024-05-10 20:28:34 +0200

    se implementa el inicio de sesion de google, recomendacion despues de cerrar sesion se debe quitar la app de segundo plano para que vuelva a iniciar con google

--- app/src/main/java/com/app/edentifica/MainActivity.kt
+++ app/src/main/java/com/app/edentifica/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.app.edentifica
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -10,13 +11,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
 import com.app.edentifica.navigation.AppNavigation
+import com.app.edentifica.navigation.AppScreen
 import com.app.edentifica.ui.theme.EDentificaTheme
 import com.app.edentifica.utils.googleAuth.GoogleAuthUiClient
 import com.app.edentifica.utils.googleAuth.SignInViewModel
@@ -53,6 +57,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    //Inicializo la el view model y los componentes necesarios para que se inicie sesion con google
+                    val navController= rememberNavController()
                     val viewModel= viewModel<SignInViewModel>()
                     val state by viewModel.state.collectAsStateWithLifecycle()
                     val launcher= rememberLauncherForActivityResult(
@@ -69,6 +75,20 @@ class MainActivity : ComponentActivity() {
 
                         }
                     )
+
+
+                    LaunchedEffect(key1 = state.isSignInSuccessful) {
+                        if(state.isSignInSuccessful){
+                            //navController.navigate(AppScreen.HomeScreen.route)
+                            Toast.makeText(
+                                applicationContext,
+                                "Sing in Successful",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+
+
                     //Este es el componente que se encarga de la navegacion y sabe cual es la primera pantalla
                     //This is the component that is in charge of navigation and knows which is the first screen.
                     AppNavigation(
